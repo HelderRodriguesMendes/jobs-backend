29a39225d0f61ab9b4b007a3ef4cf379b24db3ca
helde <helderrodrigues37@gmail.com>
2023-03-16 23:16:12 -0300

    projeto criado

---
+++ src/test/java/com/testePratico/simplesDental/SimplesDentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testePratico.simplesDental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplesDentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
